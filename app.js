ee80d7d23c4d523d9f1fe45889a53f972db6f72b
Yash Rajput <yashrajputyr10@gmail.com>
2025-01-24 14:26:23 +0530

    Adding Express js to traditional node js code

--- app.js
+++ app.js
@@ -1,12 +1,35 @@
-const fs = require('fs')
+const express = require('express')
+const bodyParser = require('body-parser');
+const app = express();
 
-const userName = 'Max';
-
-fs.writeFile('user-data.txt', 'Name: ' +userName, (err) =>{
-
-    if(err){
-        console.log(err);
+app.use((req, res, next) => {
+    let body = '';
+    req.on('end', ()=>{
+        const userName = body.split('=')[1];
+    if(userName){
+        req.body = {name: userName}
     }
+    next();
+    });
 
-    console.log("file has been written")
-})
+    req.on('data', chunk => {
+        body += chunk;
+    });
+    
+});
+
+app.use((req, res, next) => {
+
+    if(req.body){
+        return res.send('<h1>User:' +req.body.name + '</h1>')
+    }
+    res.send(`
+        <form method="POST">
+            <label for="username">Enter your name:</label>
+            <input type="text" id="username" name="username" required>
+            <button type="submit">Submit</button>
+        </form>
+    `);
+});
+
+app.listen(5000);
